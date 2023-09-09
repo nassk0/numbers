3378d62c38c2f250d27a2cece35517669e80294e
а <>
2023-09-09 07:02:51 +0500

    числа

--- main.cpp
+++ main.cpp
@@ -4,6 +4,25 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
-    return 0;
+    int  m, n = 200;
+    for (int i = 2; i<n; i++)
+    {
+        m = true;
+        for (int j = 2; j < i; j ++)
+        {
+
+                if (i % j == 0)
+                {
+                    m = false;
+                    break;
+
+                }
+
+
+        }
+        if (m)
+            cout << i << endl;
+
+    }
+
 }
